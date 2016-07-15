108da6d6b113ddcc1e5664e3c7e815a20a08d1cd
TullyNYGuy <glenn_ball@utilitek.com>
2016-07-14 20:51:18 -0400

    Started multiple word reverser program <DOESNT COMPILE!>

--- app/src/main/java/org/tullyfirst/myfirstapp/TheWordReverser.java
+++ app/src/main/java/org/tullyfirst/myfirstapp/TheWordReverser.java
@@ -10,9 +10,19 @@ public class TheWordReverser {
         char[] wordArray = word.toCharArray();
         char letter;
         String reversedWord = "";
-        for (int i=wordArray.length -1; i >= 0; i = i-1){
+        for (int i=wordArray.length -1; i >= 0; i--){
             reversedWord = reversedWord + wordArray[i];
         }
         return reversedWord;
     }
+    public String reverseMultipleWords (String word){
+        char[] wordArray = word.toCharArray();
+        int spaces = 0;
+        for (int i = 0; i < wordArray.length; i++{
+            if (wordArray[i] = " ") {
+                spaces++;
+            }
+        }
+    }
+
 }
